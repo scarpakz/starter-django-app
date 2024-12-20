964b37cd3a77c3e77b8b519b7ca25653ea94c16f
Jason Javier <jasonjjavier@gmail.com>
2024-12-20 12:35:32 -0400

    converted function based views to generic views

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import index, detail, results, vote
+from . import views
 
 app_name = "polls"
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("<int:question_id>/", detail, name="detail"),
-    path("<int:question_id>/results/", results, name="results"),
-    path("<int:question_id>/vote/", vote, name="vote"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
